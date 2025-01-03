c244052679665631e7b3551a25402276acab4bf3
onlyfanspieter <pieter.luypart@telenet.be>
2025-01-03 16:27:31 +0100

    test

---
+++ app/site/main.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", async () => {
+  const apiKey = "mz/qZWo1J4weMMk9ZaAUrg==xuJ3PaZYRZ5yPdni";
+  const apiUrl = `https://api.api-ninjas.com/v1/recipe?limit=100`;
+  const headers = { "X-Api-Key": apiKey };
+
+  const recipeContainer = document.getElementById("recipe-container");
+  const searchInput = document.getElementById("search-input");
+
+  const recipes = await (await fetch(apiUrl, { headers })).json();
+  const displayRecipes = (recipes) => {
+    recipeContainer.innerHTML = recipes
+      .map((recipe) => `<div class="recipe"><h2>${recipe.title}</h2></div>`)
+      .join("");
+  };
+
+  displayRecipes(recipes);
+
+  searchInput.addEventListener("input", (event) => {
+    const searchTerm = event.target.value.toLowerCase();
+    displayRecipes(
+      recipes.filter((recipe) =>
+        recipe.title.toLowerCase().includes(searchTerm)
+      )
+    );
+  });
+});
