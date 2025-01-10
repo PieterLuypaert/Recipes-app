77716ef85de84aa2f1f7cd3eefea7bc4509af2de
onlyfanspieter <pieter.luypart@telenet.be>
2025-01-10 12:54:02 +0100

    add server setup with Express and nodemon; configure body-parser and CORS

---
+++ app/server/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const postRouter = require("./routes/postRoutes.js");
+
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(postRouter);
+
+app.listen(PORT, () => {
+  console.log(`Onze server is aan het luisteren op http://localhost:${PORT}`);
+});
