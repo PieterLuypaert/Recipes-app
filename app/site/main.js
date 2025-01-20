c44b75c023fd52c74d8e70ec4def2937efdd6ca2
onlyfanspieter <pieter.luypart@telenet.be>
2025-01-20 18:36:05 +0100

    add delete recipe functionality and integrate into UI

--- app/site/main.js
+++ app/site/main.js
@@ -33,29 +33,6 @@ async function fetchtest() {
 
 fetchtest();
 
-recipeContainer.addEventListener("click", async (event) => {
-  if (event.target.classList.contains("delete-recipe")) {
-    const recipeId = event.target.getAttribute("data-id");
-
-    try {
-      const response = await fetch(`${apiUrl}/recipes/${recipeId}`, {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-
-      if (!response.ok) {
-        throw new Error("Failed to delete recipe");
-      }
-
-      console.log(`Deleted recipe with ID: ${recipeId}`);
-      event.target.closest(".recipe").remove();
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  }
-});
 
 // dit is mijn black en white filter
 
