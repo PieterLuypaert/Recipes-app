4cca55067b91672e1165fb583cd25c56c0c59fee
onlyfanspieter <pieter.luypart@telenet.be>
2025-01-20 20:32:07 +0100

    refactor add recipe form and improve UI elements

--- app/site/addreceipt.js
+++ app/site/addreceipt.js
@@ -7,7 +7,7 @@ document.querySelector(".add-recipe-form").addEventListener("submit", async (eve
     const ingredients = [];
     for (let i = 0; i < ingredientsInput.length; i++) {
         const [name, amount] = ingredientsInput[i].split(":").map((str) => str.trim());
-        ingredients.push({ name, amount });
+      ingredients.push({ name, amount });
     }
     const instructions = document.getElementById("instructions").value;
     const cookingTime = document.getElementById("cookingTime").value;
@@ -15,36 +15,35 @@ document.querySelector(".add-recipe-form").addEventListener("submit", async (eve
     const servings = document.getElementById("servings").value;
 
     const newRecipe = {
-        title,
-        category,
-        ingredients,
-        instructions,
-        cookingTime,
-        difficulty,
-        servings,
+      title,
+      category,
+      ingredients,
+      instructions,
+      cookingTime,
+      difficulty,
+      servings,
     };
 
     try {
-        const response = await fetch("http://localhost:3000/recipes", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(newRecipe),
-        });
+      const response = await fetch("http://localhost:3000/recipes", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newRecipe),
+      });
 
-        if (!response.ok) {
-            throw new Error("Failed to add recipe");
-        }
+      if (!response.ok) {
+        throw new Error("Failed to add recipe");
+      }
 
-        const result = await response.json();
-        console.log(result.message);
+      const result = await response.json();
+      console.log(result.message);
 
-        const messageElement = document.getElementById("message");
-        messageElement.style.display = "block";
-        messageElement.textContent = "Recept succesvol toegevoegd";
-
+      const messageElement = document.getElementById("message");
+      messageElement.style.display = "block";
+      messageElement.textContent = "Recept succesvol toegevoegd";
     } catch (error) {
-        console.error("Error:", error);
+      console.error("Error:", error);
     }
-});
+  });
